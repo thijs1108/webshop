b25e1f7899f0ae3567b1f23106d8346a5505b672
thijs1108 <t.beltman@hotmail.nl>
2016-02-16 14:20:03 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="webshopstyle.css">
+        <title>Thijs' Webshop</title>
+    </head>
+    <body>
+        <div id="content">
+            <div id="top">
+                <div id="logo">
+                    <img src="logo.jpg" width="500px" height="100px">
+                </div>
+                <div id="login">
+                    <form action="action_page.php">
+                        <input type="text" name="nickname" placeholder="Gebruikersnaam"><br>
+                        <input type="password" name="password" placeholder="Wachtwoord"><br><br>
+                        <input type="submit" value="Submit">
+                    </form>
+                </div>
+            </div>
+            <div id="text">
+                tekst
+            </div>
+        </div>
+        <div id="ad">advertentie</div>
+    </body>
+</html>
